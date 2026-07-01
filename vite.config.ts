db8204ae133dfd6964bf68bbe3fdfd5a5abbab86
Risbel Suarez <risbel961019@gmail.com>
2026-07-01 16:18:29 -0400

    refactor: Add vite-plugin-chunk-split to optimize chunk splitting in Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    chunkSplitPlugin({
+      strategy: 'default',
+      customSplitting: {
+        'react-vendor': [/node_modules[\\/]react/, /node_modules[\\/]react-dom/],
+        'router': [/node_modules[\\/]react-router-dom/],
+        'query': [/node_modules[\\/]@tanstack[\\/]react-query/],
+        'supabase': [/node_modules[\\/]@supabase/],
+        'ui': [/node_modules[\\/]@radix-ui/],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
